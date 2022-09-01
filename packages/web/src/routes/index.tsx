2c3707727b25fd9622d5830abd0ded0b948bb224
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-09-01 17:39:55 -0300

    feat(colocation): colocation

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 import { Routes as RoutesConfig, Route } from 'react-router-dom';
 
-import Home from '../src/pages/home/home';
+import Home from '../pages/home/home';
 // import ScanResults from "../pages/scanResults/ScanResults";
-import PageNotFound from '../src/pages/pageNoFound/PageNotFound';
+import PageNotFound from '../pages/pageNoFound/PageNotFound';
 // import NewScanResult from "../pages/newScanResult/NewScanResult";
-// import DetailScanResult from "../pages/detailScanResult/DetailScanResult";
+//import DetailScanResult from '../pages/detailScanResult/DetailScanResult';
 
 const Routes = () => {
   return (
     <RoutesConfig>
       <Route path='/' element={<Home />} />
-      {/* <Route path='/new' element={<NewScanResult />} />
-      <Route path='/results' element={<ScanResults />} />
-      <Route path='/results/:id' element={<DetailScanResult />} />*/}
+      {/* <Route path='/new' element={<NewScanResult />} /> */}
+      {/* <Route path='/results' element={<ScanResults />} /> */}
+      {/* <Route path='/results/:id' element={<DetailScanResult />} /> */}
       <Route path='*' element={<PageNotFound />} />
     </RoutesConfig>
   );
