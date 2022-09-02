0a42c25ff3a5d02e8b1e2f91a1ed6012e2e8b1b8
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-09-02 02:04:03 -0300

    feat(routes): fix routes

--- packages/web/service/result/result.ts
+++ packages/web/service/result/result.ts
@@ -3,31 +3,31 @@ import service from '../api';
 export const getAllResults = () =>
   service({
     method: 'GET',
-    url: `api/results`,
+    url: `api/result`,
   });
 
 export const getResult = (id: string | undefined) =>
   service({
     method: 'GET',
-    url: `api/results/${id}`,
+    url: `api/result/${id}`,
   });
 
 export const createResult = (body: any) =>
   service({
     method: 'POST',
-    url: `api/results`,
+    url: `api/result`,
     data: JSON.stringify(body),
   });
 
 export const updateResult = (id: string, body: any) =>
   service({
     method: 'PUT',
-    url: `api/results/${id}`,
+    url: `api/result/${id}`,
     data: JSON.stringify(body),
   });
 
 export const deleteResult = (id: string) =>
   service({
     method: 'DELETE',
-    url: `api/results/${id}`,
+    url: `api/result/${id}`,
   });
