01ae8664a6079968b7aa8d8b8ad21f847f85355b
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2022-09-02 02:03:15 -0300

    feat(result-get-all): add resultGetAll (#66)

--- packages/server/src/routes/resultRoutes.ts
+++ packages/server/src/routes/resultRoutes.ts
@@ -3,13 +3,13 @@ import Router from 'koa-router';
 // import { resultUpdate } from '../../api/result/resultUpdate';
 // import { resultDelete } from '../../api/result/resultDelete';
 import { resultGet } from '../api/result/resultGet';
-// import { resultGetAll } from '../../api/result/resultGetAll';
+import { resultGetAll } from '../api/result/resultGelAll';
 
 const routerResult = new Router({
   prefix: '/api/result',
 });
 
-// routerResult.get('/', resultGetAll);
+routerResult.get('/', resultGetAll);
 routerResult.get('/:id', resultGet);
 // routerResult.put('/', resultUpdate);
 // routerResult.delete('/:id', resultDelete);
